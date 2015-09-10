8f3a89bde0bdb7da0500b8c07f4687c059b8c6c8
Scott Wegner <swegner@outlook.com>
2015-09-10 16:57:57 -0700

    Initial script to query top github projects

---
+++ start.ps1
@@ -0,0 +1,44 @@
+# Query for top GitHub projects that don't yet have samples.
+
+$ErrorActionPreference = 'Stop'
+function main() {
+
+  $topProjects = queryTopProjects
+  $needSamples = $topProjects | filterProjectsWithoutSsamples
+
+  outputSummary $needSamples
+}
+
+function queryTopProjects {
+  $targetNumProjects = 100
+  $lastUpdatedFilter = [TimeSpan]::FromDays(365)
+  $urlFormat = 'https://api.github.com/search/repositories?q=pushed:>{0}&sort=stars&per_page={1}'
+
+  $githubUrl = $urlFormat -f ([DateTime]::Today - $lastUpdatedFilter).ToString('yyyy-dd-MM'), $targetNumProjects
+  $rawResults = Invoke-RestMethod -Uri $githubUrl
+  $rawProjects = $rawResults.items
+
+  $rawProjects |% {
+    New-Object PSObject -Property @{
+      Name = $_.Name
+      Url = New-Object Uri $_.html_url
+      Language = $_.language
+      Stars = $_.stargazers_count
+      Watchers = $_.watchers_count
+      LastUpdated = [DateTimeOffset]::Parse($_.pushed_at)
+    }
+  }
+}
+
+filter filterProjectsWithoutSsamples {
+  # TODO: Write filtering
+  $_
+}
+
+function outputSummary($projects) {
+  # TODO: Write output logic
+  $projects
+}
+
+
+main
