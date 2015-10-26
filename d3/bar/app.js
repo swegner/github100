573a37bbaaca64776bf1bd21c894b6c659858aff
Scott Wegner <swegner@outlook.com>
2015-10-26 07:22:20 -0700

    Convert initial boilerplate from jQuery to d3

--- d3/bar/app.js
+++ d3/bar/app.js
@@ -1,9 +1,9 @@
 // Tutorial: http://bost.ocks.org/mike/bar/
 
-(function ($) {
+(function ($, d3) {
   $(document).ready(function() {
-    var div = document.createElement('div');
-    div.innerHTML = 'Hello, world!';
-    document.body.appendChild(div);
+    var body = d3.select("body");
+    var div = body.append("div");
+    div.html("Hello, world!");
   });
-})(jQuery);
+})(jQuery, d3);
