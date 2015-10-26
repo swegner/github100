bfa6ca6b263ff4f02eebd4ddc73037d1273c80ed
Scott Wegner <swegner@outlook.com>
2015-10-26 07:13:56 -0700

    Boilerplate for D3 demo

---
+++ d3/bar/app.js
@@ -0,0 +1,7 @@
+(function ($) {
+  $(document).ready(function() {
+    var div = document.createElement('div');
+    div.innerHTML = 'Hello, world!';
+    document.body.appendChild(div);
+  });
+})(jQuery);
