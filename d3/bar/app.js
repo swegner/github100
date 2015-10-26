bf5015c9ce7b3367a98405d6a2e095697dda3a41
Scott Wegner <swegner@outlook.com>
2015-10-26 16:57:15 -0700

    Use scaling based on input data

--- d3/bar/app.js
+++ d3/bar/app.js
@@ -15,11 +15,15 @@
         .html("Hello, world!");
 
     // Generated Bar Chart
+    var scale = d3.scale.linear()
+      .domain([0, d3.max(data)])
+      .range([0, 420]);
+
     d3.select("#generatedBarChart")
       .selectAll("div")
         .data(data)
       .enter().append("div")
-        .style("width", function(d) { return d * 10 + "px"; })
+        .style("width", function(d) { return scale(d) + "px"; })
         .text(function(d) { return d; });
   });
 })(jQuery, d3);
