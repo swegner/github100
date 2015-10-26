b85be6b791d91f69aceb054fdca2754f7185eac6
Scott Wegner <swegner@outlook.com>
2015-10-26 16:53:15 -0700

    Generated bar chart

--- d3/bar/app.js
+++ d3/bar/app.js
@@ -2,13 +2,24 @@
 
 (function ($, d3) {
   "use strict";
-  
+
   $(document).ready(function() {
 
+    var data = [ 4, 3, 2, 10, 5, 7, 9 ];
+
+    // Selections!
     d3.selectAll("section")
       .append("div")
         .style("color", "white")
         .style("background-color", "black")
         .html("Hello, world!");
+
+    // Generated Bar Chart
+    d3.select("#generatedBarChart")
+      .selectAll("div")
+        .data(data)
+      .enter().append("div")
+        .style("width", function(d) { return d * 10 + "px"; })
+        .text(function(d) { return d; });
   });
 })(jQuery, d3);
