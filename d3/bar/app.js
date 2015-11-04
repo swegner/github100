9439d28e3894b6d91aabba37aa95d91043c904a1
Scott Wegner <swegner@outlook.com>
2015-11-03 17:03:03 -0800

    Add axis

--- d3/bar/app.js
+++ d3/bar/app.js
@@ -13,10 +13,10 @@
         .html("Hello, world!");
 
     var margin = {
-      top: 15,
-      left: 20,
-      right: 20,
-      bottom: 15,
+      top: 25,
+      left: 30,
+      right: 30,
+      bottom: 25,
     };
 
     var outerWidth = 500;
@@ -25,10 +25,7 @@
     var chartWidth = outerWidth - margin.left - margin.right;
     var chartHeight = outerHeight - margin.top - margin.bottom;
 
-    var generatedBarChart = d3.select("#generatedBarChart")
-      .attr("width", chartWidth);
-
-    var svgColumnChart = d3.select("#generatedSvgColumnChart")
+    var chart = d3.select("#generatedSvgColumnChart")
         .attr("width", outerWidth)
         .attr("height", outerHeight)
       .append("g")
@@ -41,43 +38,42 @@
       // Generated Bar Chart
       var maxDataVal = d3.max(data, function(d) { return d.value; });
 
-      var horizontalScale = d3.scale.linear()
-        .domain([0, maxDataVal])
-        .range([0, chartWidth]);
-
-      var verticalScale = d3.scale.linear()
+      var yScale = d3.scale.linear()
         .domain([0, maxDataVal])
         .range([chartHeight, 0]);
 
-      generatedBarChart
-        .selectAll("div")
-          .data(data)
-        .enter().append("div")
-          .style("width", function(d) { return horizontalScale(d.value) + "px"; })
-          .text(function(d) { return d.value; });
-
 
       // Generated SVG Column chart
-      var labelScale = d3.scale.ordinal()
+      var xScale = d3.scale.ordinal()
         .domain(data.map(function(d) { return d.name; }))
         .rangeRoundBands([0, chartWidth], 0.1);
 
-      var svgColSelect = svgColumnChart
-          .selectAll("g")
+      var xAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient("bottom");
+
+      var yAxis = d3.svg.axis()
+        .scale(yScale)
+        .orient("left");
+
+      chart.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + chartHeight + ")")
+        .call(xAxis);
+
+      chart.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+      var svgColSelect = chart
+          .selectAll(".bar")
             .data(data)
-          .enter().append("g")
-            .attr("transform", function(d, i) { return "translate(" + labelScale(d.name) + ",0)"; });
-
-        svgColSelect.append("rect")
-          .attr("y", function(d) { return verticalScale(d.value); })
-          .attr("width", labelScale.rangeBand())
-          .attr("height", function(d) { return chartHeight - verticalScale(d.value); });
-
-        svgColSelect.append("text")
-          .attr("x", labelScale.rangeBand() / 2)
-          .attr("dy", ".75em")
-          .attr("y", function(d) { return verticalScale(d.value) + 3; })
-          .text(function(d) { return d.value; });
+          .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return xScale(d.name); })
+            .attr("y", function(d) { return yScale(d.value); })
+            .attr("height", function(d) { return chartHeight - yScale(d.value); })
+            .attr("width", xScale.rangeBand());
       });
     });
   })(jQuery, d3);
