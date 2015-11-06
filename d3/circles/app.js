aa28c79f0bfd7e9179ddc39141755b65ce27e6e8
Scott Wegner <swegner@outlook.com>
2015-11-05 17:00:57 -0800

    Dynamically load circles

--- d3/circles/app.js
+++ d3/circles/app.js
@@ -2,9 +2,15 @@
 
 (function(d3) {
   // Selecting elements
-  var circles = d3.selectAll("circle");
-  circles
-    .style("fill", "steelblue")
-    .attr("r", 30)
-    .attr("cx", function() { return Math.random() * 720; });
+  var data = [12, 45, 72];
+  var circle = d3.select("#circle-demo");
+
+  circle.selectAll("circle")
+    .data(data)
+    .enter().append("circle")
+      .style("fill", "steelblue")
+      .attr("cy", 60)
+      .attr("cx", function(d, i) { return (720/3) * i + Math.random() * 100; })
+      .attr("r", function(d) { return Math.sqrt(d); });
+
 })(d3);
