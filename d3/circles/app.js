308a17b57b8988c649c93b9c834263f64a041807
Scott Wegner <swegner@outlook.com>
2015-11-05 16:48:17 -0800

    Modify style

--- d3/circles/app.js
+++ d3/circles/app.js
@@ -1,4 +1,9 @@
 // Circles tutorial: http://bost.ocks.org/mike/circles/
 
 (function(d3) {
+  // Selecting elements
+  var circles = d3.selectAll("circle");
+  circles
+    .style("fill", "steelblue")
+    .attr("r", 30);
 })(d3);
