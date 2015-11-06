e8305f302eba4268d76c59b5922e31dcfa26cf4e
Scott Wegner <swegner@outlook.com>
2015-11-06 09:12:39 -0800

    Add initial button with sample circle

--- d3/join/app.js
+++ d3/join/app.js
@@ -1,7 +1,25 @@
 // Join tutorial: http://bost.ocks.org/mike/join/
 
-(function(d3) {
+app = (function(d3, window) {
 
-  
+  var chartWidth = 700;
+  var chartHeight = 500;
 
-})(d3);
+  var svg = d3.select("#join-demo");
+  svg
+    .attr("width", chartWidth)
+    .attr("height", chartHeight)
+    .append("circle")
+      .attr("r", 5)
+      .attr("cx", 50)
+      .attr("cy", 50);
+
+  function renderCircles() {
+    window.alert('rendering!');
+  }
+
+  return {
+    'render': renderCircles
+  };
+
+})(d3, window);
