57757545131d220e9dd88f2977534ab8d486286f
Scott Wegner <swegner@outlook.com>
2015-11-10 16:31:57 -0800

    Build script

---
+++ jquery/about-jquery/build.ps1
@@ -0,0 +1,16 @@
+function build() {
+
+  # htmlhint: https://github.com/yaniswang/HTMLHint/
+  htmlhint.cmd
+  if ($LastExitCode -ne 0)  { return }
+
+  dir -Recurse *.js |% {
+    jshint.cmd $_.FullName
+    if ($LastExitCode -ne 0) { return }
+  }
+
+  Start-Process 'cmd.exe' '/c', 'http-server.cmd'
+  Start-Process 'http://localhost:8080/join/index.html'
+}
+
+build
---
+++ jquery/about-jquery/install.ps1
@@ -0,0 +1,3 @@
+npm install -g htmlhint
+npm install -g jshint
+npm install -g http-server
